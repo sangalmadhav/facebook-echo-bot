b62f81cde3397156347bd42f4be7d6a61d392790
garg-anuj <anujgargcse@gmail.com>
2017-12-01 18:11:56 +0000

    add rest

--- src/main/java/com/example/chatbot/FacebookBotVerticle.java
+++ src/main/java/com/example/chatbot/FacebookBotVerticle.java
@@ -2,14 +2,28 @@ package com.example.chatbot;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class FacebookBotVerticle extends AbstractVerticle {
 
   @Override
   public void start() throws Exception {
+      
+    Router router = Router.router(vertx);
+    
+    router.get("/api/whiskies").handler(this::getAll);
+    
     vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!"))
     .listen(
         Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"));
   }
+  
+  private void getAll(RoutingContext routingContext) {
+  routingContext.response()
+      .putHeader("content-type", "application/json; charset=utf-8")
+      .end("Some sample data");
+}
 }
 
